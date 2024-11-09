d7b5421a5d055b93d5d573832d2a4b96821adf7c
CarlosZoft <carlosk2gunn@hotmail.com>
2024-11-08 23:24:21 -0300

    feat: add fixed bottom button

---
+++ components/ButtonAskForHelp/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <button
+    class="w-full py-3 rounded-2xl text-white bg-[#BB0A08] hover:bg-red-600 focus:outline-none flex items-center justify-center gap-2"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
