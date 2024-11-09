63a3e233e4cbcca5e189b208b9f5927123c47909
CarlosZoft <carlosk2gunn@hotmail.com>
2024-11-08 23:25:03 -0300

    feat: add main list

--- pages/main.vue
+++ pages/main.vue
@@ -1,24 +1,9 @@
 <template>
-  <div class="flex flex-col items-center justify-center h-screen w-full">
-    <div class="flex-grow w-full">
+  <div class="flex flex-col items-center justify-center w-full">
+    <div class="flex-grow w-full pb-20">
       <div class="flex flex-row justify-between items-center w-full p-4">
-        <div class="relative flex-grow mr-2">
-          <input
-            type="text"
-            placeholder="Buscar por pedidos disponíveis"
-            class="rounded-full p-2 pl-10 w-full bg-[#E8E8E8] focus:outline-none focus:ring-2 focus:ring-gray-400 placeholder-[#52575C]"
-          />
-          <span class="absolute left-3 top-2.5 text-gray-400">
-            <img
-              src="/public/images/material-symbols_search.svg"
-              class="w-5 h-5"
-              alt="Ícone de pesquisa"
-            />
-          </span>
-        </div>
-        <button class="bg-[#E8E8E8] text-white rounded-full px-4 py-2 h-full">
-          <img src="/public/images/filters.svg" />
-        </button>
+        <SearchBar />
+        <FilterDialog />
       </div>
       <!-- Card requests paginated list -->
       <div class="flex flex-col gap-4 w-full p-4">
@@ -33,16 +18,14 @@
       </div>
     </div>
     <div class="fixed p-4 bottom-0 left-0 w-full bg-white shadow-lg">
-      <button
-        class="w-full py-3 rounded-2xl text-white bg-[#BB0A08] hover:bg-red-600 focus:outline-none flex items-center justify-center gap-2"
-      >
+      <ButtonAskForHelp>
         <img
           src="/public/images/gotinha.svg"
           class="w-6 h-6"
           alt="Ícone de coração"
         />
-        <h4 class="font-semibold">Pedir ajuda</h4>
-      </button>
+        <h4 class="font-semibold">Pedir ajuda</h4></ButtonAskForHelp
+      >
     </div>
   </div>
 </template>
@@ -76,6 +59,42 @@ const mockData = [
     requesterPhoto:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgJ9S13SnSnlFCruYgeZIVa3U6W0uA8PJV-A&s",
     bloodType: "AB-",
+  },
+  {
+    requesterName: "Lucas Pereira",
+    requesterLocal: "Hospital da Criança",
+    requesterPhoto:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQylKlGcz_m3scQl6_bzjnq3EGjIWeOWJ6rxw&s",
+    bloodType: "O-",
+  },
+
+  {
+    requesterName: "Lucas Pereira",
+    requesterLocal: "Hospital da Criança",
+    requesterPhoto:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQylKlGcz_m3scQl6_bzjnq3EGjIWeOWJ6rxw&s",
+    bloodType: "O-",
+  },
+  {
+    requesterName: "Lucas Pereira",
+    requesterLocal: "Hospital da Criança",
+    requesterPhoto:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQylKlGcz_m3scQl6_bzjnq3EGjIWeOWJ6rxw&s",
+    bloodType: "O-",
+  },
+  {
+    requesterName: "Lucas Pereira",
+    requesterLocal: "Hospital da Criança",
+    requesterPhoto:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQylKlGcz_m3scQl6_bzjnq3EGjIWeOWJ6rxw&s",
+    bloodType: "O-",
+  },
+  {
+    requesterName: "Lucas Pereira",
+    requesterLocal: "Hospital da Criança",
+    requesterPhoto:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQylKlGcz_m3scQl6_bzjnq3EGjIWeOWJ6rxw&s",
+    bloodType: "O-",
   },
   {
     requesterName: "Lucas Pereira",
