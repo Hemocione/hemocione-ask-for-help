edb76cf0e223df032f3cf53c04be31db4c68b323
CarlosZoft <carlosk2gunn@hotmail.com>
2024-11-08 23:24:48 -0300

    feat: add main search bar

---
+++ components/SearchBar/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="relative flex-grow mr-2">
+    <input
+      type="text"
+      placeholder="Buscar por pedidos disponíveis"
+      class="rounded-full p-2 pl-10 w-full bg-[#E8E8E8] focus:outline-none focus:ring-2 focus:ring-gray-400 placeholder-[#52575C]"
+    />
+    <span class="absolute left-3 top-2.5 text-gray-400">
+      <img
+        src="/public/images/material-symbols_search.svg"
+        class="w-5 h-5"
+        alt="Ícone de pesquisa"
+      />
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
