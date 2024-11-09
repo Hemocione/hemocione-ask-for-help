3ab6f0bd7bbf325bd373b4ef25de22df31f2d4c2
CarlosZoft <carlosk2gunn@hotmail.com>
2024-11-08 23:24:35 -0300

    feat: add filter dialog

---
+++ components/FilterDialog/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="bg-[#E8E8E8] text-white rounded-full px-4 py-2 h-full">
+    <img src="/public/images/filters.svg" />
+  </button>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
