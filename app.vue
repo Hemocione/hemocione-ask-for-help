93d1159bce22c87e0ad2011407b3c55d374a594a
CarlosZoft <carlosk2gunn@hotmail.com>
2024-11-08 23:24:06 -0300

    feat: add fixed header :D

--- app.vue
+++ app.vue
@@ -1,19 +1,19 @@
 <template>
   <NuxtLayout>
     <div class="content-wrapper">
+      <Header class="w-full" />
       <NuxtPage />
     </div>
   </NuxtLayout>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 <style scoped>
 .content-wrapper {
   width: 70vw;
   display: flex;
   justify-content: center;
+  flex-direction: column;
   max-width: 100%;
 }
 @media screen and (max-width: 753px) {
