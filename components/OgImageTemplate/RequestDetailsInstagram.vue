e603549226e610aad1182a4d72f532a45a43681a
Carlos Rafael <carlosk2gunn@hotmail.com>
2025-04-06 10:30:04 -0300

    feat: add og image

---
+++ components/OgImageTemplate/RequestDetailsInstagram.vue
@@ -0,0 +1,103 @@
+<template>
+  <div
+    class="flex flex-col items-center justify-center p-4 border border-[#a0a4a8] rounded-2xl"
+  >
+    <!-- logo hemocione -->
+    <img
+      src="/images/assinatura_svg_b.svg"
+      alt="Logo Hemocione"
+      class="logo_hemocione"
+      width="145"
+    />
+
+    <!-- Ajude com a sua doação! -->
+    <h3 class="font-semibold text-lg py-2">Ajude com a sua doação!</h3>
+
+    <!-- Imagem do solicitante -->
+    <div
+      class="rounded-full border border-[#B2493A] w-44 h-44 sm:!w-56 sm:!h-56"
+    >
+      <img
+        v-if="photoURL"
+        :src="photoURL!"
+        class="rounded-full"
+        alt="Imagem do usuário solicitante que precisa de doação"
+      />
+    </div>
+
+    <!-- Apelo -->
+    <p class="text-sm text-center max-w-[80%] py-2">
+      Se não puder doar, compartilhe para alcançar o maior número de pessoas!
+    </p>
+
+    <!-- Informações do solicitante -->
+    <div class="flex flex-col items-center justify-center pb-4">
+      <h2 class="font-bold text-xl text-center py-3">{{ name }}</h2>
+      <div class="flex flex-row items-center gap-1">
+        <p class="text-xs text-[#52575c]">Tipo Sanguíneo</p>
+        <div
+          class="w-8 h-4 bg-[#bb0a08] text-white rounded-full flex items-center justify-center text-xs font-medium"
+        >
+          {{ bloodType }}
+        </div>
+      </div>
+      <div class="flex flex-row items-center gap-2 pb-4">
+        <img src="/images/loc.svg" alt="Localização do usuário solicitante" />
+        <p class="text-xs text-[#52575c]">{{ location }}</p>
+      </div>
+      <div class="rounded-lg flex flex-col items-center">
+        <p class="text-base font-semibold mb-5">Tipos sanguíneos compatíveis</p>
+        <div
+          class="flex flex-wrap justify-center gap-x-4 gap-y-2 text-sm max-w-[240px]"
+        >
+          <div
+            v-for="(type, idx) in bloodTypes"
+            :key="idx"
+            class="flex items-center justify-center w-10 h-5 px-5 py-2 font-bold border rounded-2xl border-[#a0a4a8] text-[#a0a4a8]"
+            :class="{
+              'bg-[#6e91c7] border-[#6e91c7] !text-white': isCompatible(type),
+            }"
+          >
+            {{ type }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <a class="flex flex-row justify-center items-center gap-1" href="">
+      <img src="/images/instagram.svg" alt="Instagram Hemocione" />
+      <p class="text-xs text-[#bb0a08]">Hemocione</p>
+    </a>
+  </div>
+</template>
+<script setup lang="ts">
+defineOptions({
+  inheritAttrs: false,
+});
+
+const props = defineProps<{
+  name: string;
+  bloodType: BloodType;
+  location: string;
+  photoURL: string;
+}>();
+
+const bloodReceiveCompatibilities: Record<BloodType, BloodType[]> = {
+  "O-": ["O-"],
+  "O+": ["O-", "O+"],
+  "A-": ["O-", "A-"],
+  "A+": ["O-", "O+", "A-", "A+"],
+  "B-": ["O-", "B-"],
+  "B+": ["O-", "O+", "B-", "B+"],
+  "AB-": ["O-", "A-", "B-", "AB-"],
+  "AB+": ["O-", "O+", "A-", "A+", "B-", "B+", "AB-", "AB+"],
+};
+
+const bloodTypes = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"] as const;
+type BloodType = (typeof bloodTypes)[number];
+
+const isCompatible = (bloodType: BloodType) => {
+  return (
+    bloodReceiveCompatibilities[props.bloodType]?.includes(bloodType) || false
+  );
+};
+</script>
---
+++ pages/share/[id]/index.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="h-screen px-10 py-16">
+    <img
+      src="/images/x.svg"
+      alt="Botão em formato X para fechar"
+      @click="goBack"
+      class="absolute top-5 left-5"
+    />
+
+    <OgImageTemplateRequestDetailsInstagram
+      :name="request?.assisted.name"
+      :bloodType="request?.assisted.blood_type"
+      :location="request?.local_name"
+      :photoURL="request?.assisted.photo_url"
+    />
+
+    <div class="flex flex-row items-center justify-center gap-10 mt-6">
+      <div
+        @click="() => shareEvent(true)"
+        class="flex flex-col items-center justify-between gap-2"
+      >
+        <img src="/images/instagram_colorido.svg" alt="Logo do instagram" />
+        <span>Stories</span>
+      </div>
+
+      <NuxtLink
+        :to="zapUrl"
+        external
+        target="_blank"
+        class="flex flex-col items-center justify-between gap-2"
+      >
+        <img src="/images/whatsapp.svg" alt="Logo do whatsapp" />
+        <span>WhatsApp</span>
+      </NuxtLink>
+
+      <button
+        @click="() => shareEvent(false)"
+        class="flex flex-col items-center justify-between gap-2"
+      >
+        <img src="/images/plus_share.svg" alt="Icone de mais" />
+        <span>Mais</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  hideHeader: true,
+});
+
+import type { RequestWithAssisted } from "~/server/services/requestService";
+import { useRoute } from "vue-router";
+import { ref, computed } from "vue";
+
+const route = useRoute();
+const router = useRouter();
+const id = route.params.id as string;
+const request = ref<RequestWithAssisted | null>(null);
+
+const goBack = () => {
+  router.back();
+};
+
+request.value = await $fetch(`/api/request/${id}`, {
+  method: "GET",
+});
+
+const currentUrl = ref("");
+
+const instagramImageBlob = ref<Blob | null>(null);
+const instagramImageLocalUrl = ref<string | null>(null);
+const instagramImageUrl = ref<string | null>(null);
+const shareUrl = ref<string | null>(null);
+
+onMounted(async () => {
+  currentUrl.value = window.location.href;
+  const baseUrl = window.location.origin;
+
+  shareUrl.value = `${baseUrl}/description/${id}`;
+
+  instagramImageUrl.value = `${baseUrl}/__og-image__/image/share/${id}/instagram/og.png`;
+
+  instagramImageBlob.value = await fetch(instagramImageUrl.value).then((res) =>
+    res.blob()
+  );
+  if (!instagramImageBlob.value) return;
+
+  instagramImageLocalUrl.value = URL.createObjectURL(instagramImageBlob.value);
+});
+
+const shareText = `${request.value?.assisted.name} precisa da nossa ajuda! Vamos salvar sua vida juntos?`;
+const zapUrl = computed(() =>
+  shareUrl.value
+    ? getWhatsappUrl(shareText, shareUrl.value)
+    : "#"
+);
+
+async function shareEvent(withImage: boolean = false) {
+  try {
+    const data: {
+      title: string;
+      text: string;
+      url: string;
+      files?: File[];
+    } = {
+      title: `Hemocione - ${request.value?.assisted.name.split(" ")[0]}`,
+      text: "Estou precisando da sua ajuda!",
+      url: shareUrl.value!,
+    };
+
+    if (withImage && instagramImageBlob.value) {
+      const instagramImageFile = new File(
+        [instagramImageBlob.value],
+        "instagram.png",
+        {
+          type: "image/png",
+        }
+      );
+      data.files = [instagramImageFile];
+    }
+
+    const navigatorShareable = Boolean(navigator.canShare);
+
+    if (navigatorShareable && navigator.canShare(data)) {
+      await navigator.share(data);
+    } else {
+      navigator.clipboard.writeText(shareUrl.value!);
+      ElMessage({
+        message:
+          "O link do pedido de ajuda foi copiado para a área de transferência.",
+        type: "success",
+      });
+    }
+  } catch (error) {
+    ElMessage({
+      message: "Não foi possível compartilhar o pedido de ajuda.",
+      type: "error",
+    });
+  }
+}
+</script>
