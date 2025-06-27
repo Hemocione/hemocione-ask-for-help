db6637390d90a8c5bc29a0684b06b20cbbaa0a28
Gabriel Palma Javaroni <gabrieljavaroni2@gmail.com>
2025-06-27 00:24:18 -0300

    Added date as query parameter to syncjob get endpoint

--- server/api/request/syncjob.get.ts
+++ server/api/request/syncjob.get.ts
@@ -1,25 +1,69 @@
+import z from "zod";
+import { paginateListRequest } from "~/server/services/requestService";
+import { bloodTypes } from "~/types/blood";
+import {
+  bloodTypeToDbType,
+  BloodTypeValues,
+} from "~/utils/bloodTypeTransformation";
 
-export default defineEventHandler(async () => {
-  try {
-    const pedidos = await prisma.pedido.findMany({
-      orderBy: {
-        criado_em: 'desc',
-      },
-    })
-
-    return {
-      status: 'success',
-      data: pedidos,
-    }
-  } catch (error) {
-    console.error('Erro ao buscar pedidos:', error)
-
-    return sendError(
-      createError({
-        statusCode: 500,
-        statusMessage: 'Erro ao buscar pedidos',
-      })
+const ListRequestSchema = z.object({
+  page: z
+    .string()
+    .transform((str) => Number(str))
+    .optional(),
+  per_page: z
+    .string()
+    .transform((str) => Number(str))
+    .optional(),
+  name: z.string().optional(),
+  bloodTypes: z
+    .preprocess((val) => (typeof val === "string" ? [val] : val), 
+      z.array(
+        z
+          .enum(bloodTypes)
+          .transform(bloodTypeToDbType)
+      )
     )
-  }
-})
+    .optional(),
+  last: z
+    .string()
+    .transform((str) => new Date(str))
+    .refine((date)=> !isNaN(date.getTime()), {
+	message: "Invalid date",
+	})
+    .optional(),
 
+});
+
+export type Request = z.infer<typeof ListRequestSchema>;
+
+export default defineEventHandler(async (event) => {
+  const { page, per_page, bloodTypes, name, last } = await getValidatedQuery(
+    event,
+    ListRequestSchema.parse
+  );
+
+  const query: {
+    name?: string;
+    bloodTypes?: BloodTypeValues[];
+    last?:Date; 
+  } = {};
+
+  if (name) {
+    query.name = name;
+  }
+
+  if (bloodTypes) {
+    query.bloodTypes = bloodTypes.filter((e) => e !== undefined);
+  }
+
+  if (last){
+    query.last = last;
+  }
+
+  return await paginateListRequest({
+    page,
+    per_page,
+    query,
+  });
+});
